9836aa3776cd0819ab31540d1764f205003d3acc
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2025-09-10 10:33:24 -0300

    refactor: atualiza o controller para retornar uma mensagem em português e define a rota como 'api'

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 
-@Controller()
+@Controller('api')
 export class AppController {
   constructor() {}
 
   @Get()
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): { message: string } {
+    return { message: 'Olá Mundo!' };
   }
 }
